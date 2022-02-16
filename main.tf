575f6dc5ba00d9aac3eddda5250179b20e453b2c
drewmullen <drmullen@amazon.com>
2022-02-16 13:17:14 -0500

    use netmask instead of new_bits

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 locals {
-  addrs_by_idx  = cidrsubnets(var.base_cidr_block, var.networks[*].new_bits...)
-  addrs_by_name = { for i, n in var.networks : n.name => local.addrs_by_idx[i] if n.name != null }
-  network_objs = [for i, n in var.networks : {
+  cidr_netmask = tonumber(split("/", var.base_cidr_block)[1])
+  networks_netmask_to_bits = [for i, v in var.networks: { "name" = v.name, "new_bits" = tonumber(v.netmask - local.cidr_netmask) } ]
+
+  addrs_by_idx  = cidrsubnets(var.base_cidr_block, local.networks_netmask_to_bits[*].new_bits...)
+  addrs_by_name = { for i, n in local.networks_netmask_to_bits : n.name => local.addrs_by_idx[i] if n.name != null }
+  network_objs = [for i, n in local.networks_netmask_to_bits : {
     name       = n.name
-    new_bits   = n.new_bits
+    netmask    = var.networks[i].netmask
+    bits       = n.new_bits
     cidr_block = n.name != null ? local.addrs_by_idx[i] : tostring(null)
   }]
 }
